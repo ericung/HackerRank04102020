301b3648f5caca7aa37d900da612249602924a1b
Eric Ung <ungericw@tutanota.com>
2020-04-10 11:14:08 -0500

    Picking myself back up again

---
+++ BreadthFirstSearch/BreadthFirstSearch.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreadthFirstSearch
+{
+  class BreadthFirstSearch
+  {
+    public static void Search(Node root)
+    {
+      Queue<Node> queue = new Queue<Node>();
+      root.Data = 1;
+      queue.Enqueue(root);
+
+      while(queue.Count() != 0)
+      {
+        Node r = queue.Dequeue();
+        r.Data = 1;
+
+        Console.WriteLine("Id: " + r.Id);
+
+        if(r.Left != null)
+        {
+          if (r.Left.Data == 0)
+          {
+            r.Left.Data = 1;
+            queue.Enqueue(r.Left);
+          }
+        }
+
+        if (r.Right != null)
+        {
+          if (r.Right.Data == 0)
+          {
+            r.Right.Data = 1;
+            queue.Enqueue(r.Right);
+          }
+        }
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      Node node1 = new Node();
+      Node node2 = new Node();
+      Node node3 = new Node();
+      Node node4 = new Node();
+      Node node5 = new Node();
+      Node node6 = new Node();
+      Node node7 = new Node();
+
+      node1.Left = node2;
+      node1.Right = node3;
+      node2.Left = node4;
+      node2.Right = node5;
+      node3.Left = node6;
+      node3.Right = node7;
+
+      Search(node1);
+    }
+  }
+}
---
+++ BreadthFirstSearch/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreadthFirstSearch
+{
+
+  public class Node
+  {
+    public static int Identifier = 0;
+    public int Id;
+    public int Data;
+    public Node Left;
+    public Node Right;
+
+    public Node()
+    {
+      Data = 0;
+
+      Id = Identifier;
+      Identifier++;
+    }
+
+    public Node(int data, Node next, Node previous)
+    {
+      Data = data;
+      Left = next;
+      Right = previous;
+
+      Id = Identifier;
+      Identifier++;
+    }
+  }
+}
