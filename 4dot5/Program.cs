7dbb98ef8480ee531f1fb6a00b37c051358d6686
Eric Ung <ungericw@tutanota.com>
2020-04-25 10:39:57 -0500

    4/25/2020 4dot5

---
+++ 4dot5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4dot5
+{
+  class Program
+  {
+    public static Boolean ValidateBinarySearchTree(Node head)
+    {
+      return ValidateBinarySearchTree(head, null, null);
+    }
+
+    public static Boolean ValidateBinarySearchTree(Node head, int? min, int? max)
+    {
+      if (head == null)
+      {
+        return true;
+      }
+
+      if (((min != null)&&(head.Value <= min))||((max != null)&&(head.Value > max)))
+      {
+        return false;
+      }
+
+      if ((!ValidateBinarySearchTree(head.Left, min, head.Value))||(!ValidateBinarySearchTree(head.Right, head.Value, max)))
+      {
+        return false;
+      }
+
+      return true;
+    }
+
+    static void Main(string[] args)
+    {
+      int[] array1 = { 1, 2, 4, 5, 6, 7, 10, 11, 23, 24, 27, 29, 39, 41 };
+      Node head1 = Helper.CreateMinimalBST(array1);
+
+      Helper.Print(head1);
+      Console.WriteLine(ValidateBinarySearchTree(head1));
+
+      Console.WriteLine("=======================================================");
+      Node head1r = Helper.CreateMinimalBSTRecursive(array1, 0, array1.Length - 1);
+
+      Helper.Print(head1r);
+      Console.WriteLine(ValidateBinarySearchTree(head1r));
+    }
+  }
+}
