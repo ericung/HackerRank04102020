93cd2791dc498f08e523028b08337835c0991c5b
Eric Ung <ungericw@tutanota.com>
2020-04-25 11:14:50 -0500

    4/25/2020 4dot6

---
+++ 4dot6/Program.cs
@@ -0,0 +1,51 @@
+﻿using ChapterFour;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4dot6
+{
+  class Program
+  {
+    public static Node Successor(Node node)
+    {
+      Node successor = node;
+
+      if (node.Right != null)
+      {
+        successor = node.Right;
+
+        while(successor.Left != null)
+        {
+          successor = successor.Left;
+        }
+
+        return successor;
+      }
+      else if (node.Parent != null)
+      {
+        successor = node.Parent;
+        
+        while(successor.Parent != null)
+        {
+          successor = successor.Parent;
+        }
+
+        return successor;
+      }
+
+      return node;
+    }
+
+    static void Main(string[] args)
+    {
+      int[] array1 = { 1, 2, 4, 5, 6, 7, 10, 11, 23, 24, 27, 29, 39, 41 };
+      Node head1 = Node.CreateMinimalBST(array1);
+
+      Node.Print(head1);
+      Console.WriteLine(Successor(head1.Left).Value);
+    }
+  }
+}
