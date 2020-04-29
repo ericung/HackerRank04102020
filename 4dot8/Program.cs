afae7e070c3163c31e14e6f85aa6befd9024de6f
Eric Ung <ungericw@tutanota.com>
2020-04-29 15:33:05 -0500

    4/30/2020 Optimized solution for 4dot8

--- 4dot8/Program.cs
+++ 4dot8/Program.cs
@@ -8,9 +8,22 @@ using System.Threading.Tasks;
 
 namespace _4dot8
 {
+  class Result
+  {
+    public Node Node;
+    public Boolean IsAncestor;
+    
+    public Result(Node n, Boolean isAnc)
+    {
+      Node = n;
+      IsAncestor = isAnc;
+    }
+  }
   class Program
   {
-    public static Node FindCommonAncestor(Node a, Node b)
+    // This is my solution.
+    // Take the hashmap and 
+    public static Node FindCommonAncestorMySolution(Node a, Node b)
     {
       if ((a == null)||(b == null))
       {
@@ -43,13 +56,91 @@ namespace _4dot8
       return commonAncestor;
     }
 
+    public static Node FindCommonAncestor(Node root, Node p, Node q)
+    {
+      Result r = CommonAncestorHelper(root, p, q);
+      Console.WriteLine(r.Node.Value + " : " + r.IsAncestor);
+      if (r.IsAncestor)
+      {
+        return r.Node;
+      }
+
+      return null;
+    }
+
+    public static Result CommonAncestorHelper(Node root, Node p, Node q)
+    {
+      if (root == null)
+      {
+        return new Result(null, false);
+      }
+
+      if (root == p && root == q)
+      {
+        return new Result(root, true);
+      }
+
+      Console.WriteLine("root: "+ root.Value + "| a: " + p.Value + " | b: " + q.Value);
+
+      Result rx = CommonAncestorHelper(root.Left, p, q);
+      if (rx.IsAncestor)
+      {
+        return rx;
+      }
+
+      Result ry = CommonAncestorHelper(root.Right, p, q);
+      if (ry.IsAncestor)
+      {
+        return ry;
+      }
+
+      if (rx.Node != null && ry.Node != null)
+      {
+        return new Result(root, true); // This is the common ancestor.
+      }
+      else if (root == p || root == q)
+      {
+        /* If we're currently at p or q, and we also found one of those nodes in a 
+         * subtree, then this is truly an ancestor and the flag should be true.
+         */
+        Boolean isAncestor = rx.Node != null || ry.Node != null;
+        if (rx.Node != null)
+        {
+          Console.WriteLine("2rx: " + rx.Node.Value + " : " + rx.IsAncestor + " : " + isAncestor);
+        }
+
+        if (ry.Node != null)
+        {
+          Console.WriteLine("2ry: " + ry.Node.Value + " : " + ry.IsAncestor + " : " + isAncestor);
+        }
+        return new Result(root, isAncestor);
+      }
+      else
+      {
+        return new Result(rx.Node != null ? rx.Node : ry.Node, false);
+      }
+    }
+
     static void Main(string[] args)
     {
-      int[] array1 = { 1, 2, 4, 5, 6, 7, 10, 11, 23, 24, 27, 29, 39, 41 };
+      int[] array1 = { 1, 2, 4, 5, 6, 7, 10, 11, 23, 24, 27, 29, 39, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 57, 68, 69 };
       Node head1 = Node.CreateMinimalBST(array1);
 
       Node.Print(head1);
-      Console.WriteLine(FindCommonAncestor(head1.Left.Left, head1.Left.Right.Right).Value);
+      Console.WriteLine("My solution: " + FindCommonAncestorMySolution(head1.Left.Left, head1.Left.Right.Right).Value);
+
+
+      Node a = head1.Left.Right.Left.Left;
+      Node b = head1.Left.Right.Right.Right;
+
+      Console.WriteLine("Book solution: " + FindCommonAncestor(head1, a, b).Value);
+      Console.WriteLine("========================================");
+      Node a2 = head1.Right.Left.Right;
+      Node b2 = head1.Right.Left.Left;
+      // Node a2 = head1.Left.Left;
+      // Node b2 = head1.Left.Left.Right;
+      Console.WriteLine("Book solution: " + FindCommonAncestor(head1, a2, b2).Value);
+
     }
   }
 }
---
+++ 4dot9/Program.cs
@@ -0,0 +1,133 @@
+﻿using ChapterFour;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4dot9
+{
+  class Program
+  {
+    public static List<LinkedList<int>> AllSequence(Node node)
+    {
+      List<LinkedList<int>> result = new List<LinkedList<int>>();
+
+      if (node == null)
+      {
+        result.Add(new LinkedList<int>());
+        return result;
+      }
+
+      LinkedList<int> prefix = new LinkedList<int>();
+      prefix.AddLast(node.Value);
+
+      /* Recurse on left and right subtrees */
+      List<LinkedList<int>> leftSeq = AllSequence(node.Left);
+      List<LinkedList<int>> rightSeq = AllSequence(node.Right);
+
+      /* Weave together each list from the left and right sides. */
+      foreach (LinkedList<int> left in leftSeq)
+      {
+        foreach (LinkedList<int> right in rightSeq)
+        {
+          List<LinkedList<int>> weaved = new List<LinkedList<int>>();
+          weaveLists(left, right, weaved, prefix);
+          result.AddRange(weaved);
+        }
+      }
+      return result;
+    }
+
+    /* Weave lists together in all possible ways. This algorithm works by removing the
+     from one list, recursing, and then doing the same thing with the other list.*/
+    public static void weaveLists(LinkedList<int> first, LinkedList<int> second, List<LinkedList<int>> results, LinkedList<int> prefix)
+    {
+      /* One list is empty. Add remainder to [a cloned] prefix and store result. */
+      if (first.Count == 0 || second.Count == 0)
+      {
+        int[] prefixArray = new int[prefix.Count];
+        prefix.CopyTo(prefixArray, 0);
+        LinkedList<int> result = new LinkedList<int>(prefixArray);
+        foreach(var item in first)
+        {
+          result.AddLast(item);
+        }
+        foreach(var item in second)
+        {
+          result.AddLast(item);
+        }
+        results.Add(result);
+        return;
+      }
+
+      /* Recurse with head of first added to the prefix. Removing the head will damage
+       * first, so we'll need to put it back where we found it afterwards. */
+      int headFirst = first.First();
+      first.RemoveFirst();
+      prefix.AddLast(headFirst);
+      weaveLists(first, second, results, prefix);
+      prefix.RemoveLast();
+      first.AddFirst(headFirst);
+
+      /* Do the same thing with second, damging and then restoring the list. */
+      int headSecond = second.First();
+      second.RemoveFirst();
+      prefix.AddLast(headSecond);
+      weaveLists(first, second, results, prefix);
+      prefix.RemoveLast();
+      second.AddFirst(headSecond);
+    }
+
+    public static void PrintSequence(List<LinkedList<int>> sequences)
+    {
+      foreach(var linkedList in sequences)
+      {
+        int count = 0;
+        String str = String.Empty;
+        foreach(var item in linkedList)
+        {
+          str += item;
+          
+          if (count < linkedList.Count - 1)
+          {
+            str += " ";
+          }
+
+          count++;
+        }
+
+        Console.WriteLine(str);
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      //int[] array1 = { 1, 2, 4, 5, 6, 7, 10, 11, 23, 24, 27, 29, 39, 41 };
+      int[] array1 = { 1, 2, 3, 4 };
+      Node head1 = Node.CreateMinimalBST(array1);
+
+      Node.Print(head1);
+
+      List<LinkedList<int>> sequences = AllSequence(head1);
+
+      PrintSequence(sequences);
+      
+      Console.WriteLine("================================================");
+
+      Node head2 = new Node();
+      Node head2Left = new Node();
+      Node head2Right = new Node();
+
+      head2.Left = head2Left;
+      head2.Right = head2Right;
+
+      head2.Value = 2;
+      head2Left.Value = 1;
+      head2Right.Value = 3;
+
+      List<LinkedList<int>> sequences2 = AllSequence(head2);
+
+      PrintSequence(sequences2);
+
+    }
+  }
+}
