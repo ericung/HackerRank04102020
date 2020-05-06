fcaa79097187a90a4a491a640226453403ec9079
Eric Ung <ungericw@tutanota.com>
2020-05-06 16:44:09 -0500

    5/6/2020 8dot2 and 8dot3

---
+++ ChapterEight/8dot2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace _8dot2
+{
+  class Point
+  {
+    public int X;
+    public int Y;
+
+    public Point(int x, int y)
+    {
+      X = x;
+      Y = y;
+    }
+  }
+
+  class Program
+  {
+    public static List<Point> getPath(Boolean[][] maze)
+    {
+      if ((maze == null)||(maze.Length == 0))
+      {
+        return null;
+      }
+
+      List<Point> path = new List<Point>();
+      HashSet<Point> failedPoints = new HashSet<Point>();
+      if (getPath(maze, maze.Length - 1, maze[0].Length - 1, path, failedPoints))
+      {
+        return path;
+      }
+
+      return null;
+    }
+
+    public static Boolean getPath(Boolean[][] maze, int row, int col, List<Point> path, HashSet<Point> failedPoints)
+    {
+      /* If out of bounds or not available, return */
+      if ((col < 0)||(row < 0)||(maze[row][col]))
+      {
+        return false;
+      }
+
+      Point p = new Point(row, col);
+
+      /* If we've already visited this cell, return. */
+      if (failedPoints.Contains(p))
+      {
+        return false;
+      }
+
+      Boolean isAtOrigin = (row == 0) && (col == 0);
+
+      /* If there's a path from start to my current location, add my location */
+      if ((isAtOrigin) || (getPath(maze, row, col-1, path, failedPoints)) || (getPath(maze, row-1, col, path, failedPoints)))
+      {
+        path.Add(p);
+        return true;
+      }
+
+      failedPoints.Add(p);
+      return false;
+    }
+
+
+    static void Main(string[] args)
+    {
+      Boolean[][] maze = new Boolean[100][];
+      
+      for(int i = 0; i < 100; i++)
+      {
+        maze[i] = new Boolean[100];
+        for(int j = 0; j < 100; j++)
+        {
+          maze[i][j] = false;
+        }
+      }
+
+      List<Point> path = getPath(maze);
+
+      for(int i = 0; i < path.Count; i++)
+      {
+        Console.WriteLine(path[i].X + " " + path[i].Y);
+      }
+    }
+  }
+}
---
+++ ChapterEight/8dot3/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8dot3
+{
+  class Program
+  {
+
+    public static int? findMagicIndex(int[] array)
+    {
+      for(int i = 0; i < array.Length; i++)
+      {
+        if (array[i] == i)
+        {
+          return i;
+        }
+      }
+
+      return null;
+    }
+
+    public static int magicFast(int[] array)
+    {
+      return magicFast(array, 0, array.Length - 1);
+    }
+
+    public static int magicFast(int[] array, int start, int end)
+    {
+      if (end < start) return -1;
+
+      int midIndex = (start + end) / 2;
+      int midValue = array[midIndex];
+      if (midValue == midIndex)
+      {
+        return midIndex;
+      }
+
+      /* Search left */
+      int leftIndex = Math.Min(midIndex - 1, midValue);
+      int left = magicFast(array, start, leftIndex);
+      if (left >= 0)
+      {
+        return left;
+      }
+
+      /* Search right */
+      int rightIndex = Math.Max(midIndex + 1, midValue);
+      int right = magicFast(array, rightIndex, end);
+
+      return right;
+    }
+
+    static void Main(string[] args)
+    {
+      //  must have negative integers or it is the identity
+      int[] array = { -33, 0, 2, 99, 101};
+
+      int? magicIndex = findMagicIndex(array);
+
+      string print = magicIndex != null ? magicIndex.ToString() : "null";
+
+      Console.WriteLine(print);
+      Console.WriteLine(magicFast(array));
+      Console.WriteLine("o=====================================================");
+
+      // don't need negative integers
+      int[] array2 = { 1, 2, 2, 55, 66, 77 };
+
+      int? magicIndex2 = findMagicIndex(array2);
+
+      string print2 = magicIndex2 != null ? magicIndex2.ToString() : "null";
+
+      Console.WriteLine(print2);
+      Console.WriteLine(magicFast(array2));
+    }
+  }
+}
