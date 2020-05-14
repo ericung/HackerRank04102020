d335f1609d86725efe0b624f5016fe91d5964061
Eric Ung <ungericw@tutanota.com>
2020-05-14 18:10:44 -0500

    5/14/2020 8dot8

--- ChapterEight/8dot8/Program.cs
+++ ChapterEight/8dot8/Program.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _8dot8
 {
   class Program
   {
-    List<String> getPerms(String remainder)
+    public static List<String> getPerms(String remainder)
     {
       int len = remainder.Length;
       List<String> result = new List<String>();
+      HashSet<String> exists = new HashSet<String>();
 
       if (len == 0)
       {
@@ -23,20 +21,43 @@ namespace _8dot8
       {
         /* Remove char i and find  permutations of remaining chars. */
         String before = remainder.Substring(0, i);
-        String after = remainder.Substring(i + 1, len);
+        int endLength = len - i - 1;
+        String after;
+        if (endLength > 0)
+        {
+          after = remainder.Substring(i + 1, endLength);
+        }
+        else
+        {
+          after = String.Empty;
+        }
         List<String> partials = getPerms(before + after);
 
         /* Prepend char i to each permutation. */
         foreach (String s in partials)
         {
-          result.Add(remainder[i] + s);
+          if (!exists.Contains(remainder[i] + s))
+          {
+            exists.Add(remainder[i] + s);
+            result.Add(remainder[i] + s);
+
+          }
         }
       }
 
       return result;
     }
+
     static void Main(string[] args)
     {
+      var result = getPerms("haya");
+
+      foreach(var item in result)
+      {
+        Console.WriteLine(item);
+      }
+
+      Console.WriteLine(result.Count);
     }
   }
 }
