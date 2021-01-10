260b4aeb3e7699f8cab9656d59ad79daff3157d1
Eric Ung <ungericw@tutanota.com>
2021-01-10 17:46:20 -0600

    1/10/2021 Review leetcode and hackerrank

---
+++ Playground01102021/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Playground01102021
+{
+  class Program
+  {
+
+    public void fizzBuzz(int[] array)
+    {
+      for (int i = 0; i < array.Length; i++) {
+        if ((array[i] % 3 == 0)&&(array[i] % 5 == 0)) {
+          Console.WriteLine("fizz buzz");
+        }
+        else if (array[i] % 3 == 0)
+        {
+          Console.WriteLine("fizz");
+        }
+        else if (array[i]%5 == 0)
+        {
+          Console.WriteLine("buzz");
+        } else
+        {
+          Console.WriteLine(array[i]);
+        }
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+      Program program = new Program();
+
+      program.fizzBuzz(array);
+    }
+  }
+}
